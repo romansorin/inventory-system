05fb72879a2b306ed64d79b7b53c76b2d888dbcc
Roman Sorin <rmaximsorin@gmail.com>
2021-04-18 00:36:32 -0400

    Use classes  to clean up API views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    path('items/', views.item_list),
-    path('items/<int:pk>', views.item_detail)
+    path('items/', views.ItemList.as_view()),
+    path('items/<int:pk>', views.ItemDetail.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,51 +1,19 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from .models import Item
 from .serializers import ItemSerializer
+from rest_framework import generics
 
 
-@csrf_exempt
-def item_list(request):
+class ItemList(generics.ListCreateAPIView):
     """
     List all items or create a new item.
     """
-    if request.method == 'GET':
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ItemSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
 
-@csrf_exempt
-def item_detail(request, pk):
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Get, update, or delete an item.
     """
-    try:
-        item = Item.objects.get(pk=pk)
-    except Item.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = ItemSerializer(item)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ItemSerializer(item, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        item.delete()
-        return HttpResponse(status=204)
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
