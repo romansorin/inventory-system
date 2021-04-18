cf1b80c1b484e72158698d5dc94c62d567291672
Roman Sorin <rmaximsorin@gmail.com>
2021-04-18 01:01:43 -0400

    Add nested Category relationship to Item return

--- api/serializers.py
+++ api/serializers.py
@@ -2,20 +2,22 @@ from rest_framework import serializers
 from .models import Item, Location, Category
 
 
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = ['id', 'description', 'in_use', 'replacement_link',
-                  'replacement_cost', 'created_at', 'updated_at']
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['name']
+        fields = ['id', 'name', 'created_at', 'updated_at']
 
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
-        fields = ['name']
+        fields = ['id', 'name', 'created_at', 'updated_at']
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = Item
+        fields = ['id', 'description', 'in_use', 'replacement_link',
+                  'replacement_cost', 'created_at', 'updated_at', 'category']
