ac0e63f62947949433fe3eb98645ad4a440b7e8b
Roman Sorin <rmaximsorin@gmail.com>
2021-04-18 12:03:39 -0400

    Regenerate migrations, set fields as optional for inclusion

--- api/serializers.py
+++ api/serializers.py
@@ -18,8 +18,10 @@ class LocationSerializer(serializers.ModelSerializer):
 
 
 class ItemSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(allow_null=True, read_only=False)
-    location = LocationSerializer(allow_null=True, read_only=False)
+    category = CategorySerializer(
+        allow_null=True, required=False, read_only=False)
+    location = LocationSerializer(
+        allow_null=True, required=False, read_only=False)
 
     class Meta:
         model = Item
@@ -27,16 +29,29 @@ class ItemSerializer(serializers.ModelSerializer):
                   'replacement_cost', 'created_at', 'updated_at', 'category', 'location']
 
     def create(self, validated_data):
-        location_data = validated_data.pop('location')
-        category_data = validated_data.pop('category')
+        category_data = None
+        location_data = None
+        if validated_data.get('location'):
+            location_data = validated_data.pop('location')
+        if validated_data.get('category'):
+            category_data = validated_data.pop('category')
         item = Item.objects.create(**validated_data)
 
         # Note/TODO: running into an issue where I cannot provide a null category/location field to unset/prevent setting the relation, despite having null and blank fields defined
         # I do not always want to attach a location/category to an item, either because it doesn't fit some level of granulity, or it just doesn't exist yet
-        category = Category.objects.filter(name=category_data['name']).first()
-        if category is None and category_data.get('name') is not None:
-            category = Category.objects.create(**category_data)
-        category.item_set.add(item)
+        if category_data is not None:
+            category = Category.objects.filter(
+                name=category_data['name']).first()
+            if category is None and category_data.get('name') is not None:
+                category = Category.objects.create(**category_data)
+            category.item_set.add(item)
+
+        if location_data is not None:
+            location = Location.objects.filter(
+                name=location_data['name']).first()
+            if location is None and location_data.get('name') is not None:
+                location = Location.objects.create(**location_data)
+            location.item_set.add(item)
 
         return item
 
