b38cb3ab77c6edf8a07a926bb88e6e8931fb1ec2
Roman Sorin <rmaximsorin@gmail.com>
2021-04-18 00:09:38 -0400

    Added other methods for Item endpoint

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('items/', views.item_list)
+    path('items/', views.item_list),
+    path('items/<int:pk>', views.item_detail)
 ]
--- api/views.py
+++ api/views.py
@@ -22,3 +22,30 @@ def item_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def item_detail(request, pk):
+    """
+    Get, update, or delete an item.
+    """
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ItemSerializer(item)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ItemSerializer(item, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        item.delete()
+        return HttpResponse(status=204)
