1a16d24d8a3382a7fdd54b3d1df98e0fe87c0b65
Roman Sorin <rmaximsorin@gmail.com>
2021-04-18 02:09:01 -0400

    Update migration, item models

--- api/models.py
+++ api/models.py
@@ -8,9 +8,9 @@ class Item(models.Model):
     replacement_cost = models.DecimalField(
         max_digits=19, decimal_places=2, null=True)
     category = models.ForeignKey(
-        'Category', null=True, on_delete=models.SET_NULL)
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     location = models.ForeignKey(
-        'Location', null=True, on_delete=models.SET_NULL)
+        'Location', null=True, blank=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,8 @@
+import logging
 from rest_framework import serializers
 from .models import Item, Location, Category
+
+logger = logging.getLogger(__name__)
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -15,10 +18,47 @@ class LocationSerializer(serializers.ModelSerializer):
 
 
 class ItemSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
-    location = LocationSerializer(read_only=True)
+    category = CategorySerializer(allow_null=True, read_only=False)
+    location = LocationSerializer(allow_null=True, read_only=False)
 
     class Meta:
         model = Item
         fields = ['id', 'description', 'in_use', 'replacement_link',
                   'replacement_cost', 'created_at', 'updated_at', 'category', 'location']
+
+    def create(self, validated_data):
+        location_data = validated_data.pop('location')
+        category_data = validated_data.pop('category')
+        item = Item.objects.create(**validated_data)
+
+        # Note/TODO: running into an issue where I cannot provide a null category/location field to unset/prevent setting the relation, despite having null and blank fields defined
+        category = Category.objects.filter(name=category_data['name']).first()
+        if category is None and category_data.get('name') is not None:
+            category = Category.objects.create(**category_data)
+        category.item_set.add(item)
+
+        return item
+
+    def update(self, instance, validated_data):
+        location_data = validated_data.pop('location')
+        category_data = validated_data.pop('category')
+
+        # If the location field is provided, we want to:
+        # 1. Check if the instance has an existing location relationship
+        # 2. If it does, check if the location name and relationship match
+        # 3. If they don't match, find the provided location and attach it
+
+        location = Location.objects.filter(
+            name=location_data['location']).first()
+
+        # If the location field is not provided, remove the relationship entirely
+
+        instance.description = validated_data.get(
+            'description', instance.description)
+        instance.in_use = validated_data.get('in_use', instance.in_use)
+        instance.replacement_link = validated_data.get(
+            'replacement_link', instance.replacement_link)
+        instance.replacement_cost = validated_data.get(
+            'replacement_link', instance.replacement_cost)
+
+        return instance
